1724a38fe6b39649a346529833374b4f8611e914
Elabsurdo984 <matiassfernandez00@gmail.com>
2025-12-29 19:27:43 -0300

    animacion del personaje

--- scenes/suelo/suelo.gd
+++ scenes/suelo/suelo.gd
@@ -1,6 +1,11 @@
 extends TileMapLayer
 
 @export var speed := 200.0
+@export var loop_width := 2000.0  # Ancho del bucle en píxeles
 
 func _process(delta):
 	position.x -= speed * delta
+	
+	# Cuando se mueve demasiado a la izquierda, resetear posición
+	if position.x <= -loop_width:
+		position.x += loop_width
